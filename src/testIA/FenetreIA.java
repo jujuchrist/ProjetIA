f7f5e5491941825b65197dcad5ac2bae243b77dc
Julien Bénard <jujuchrist@live.fr>
2015-11-04 02:39:47 +0100

    Second commit

---
+++ src/testIA/FenetreIA.java
@@ -0,0 +1,12 @@
+package testIA;
+
+import javax.swing.JFrame;
+
+public class FenetreIA extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/testIA/MainClass.java
+++ src/testIA/MainClass.java
@@ -1,10 +1,11 @@
 package testIA;
 
+import javax.swing.JFrame;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame fenetre = new FenetreIA();
 	}
 
 }
